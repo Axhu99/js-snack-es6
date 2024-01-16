a8f139d6087e514e6eccc96d37d57ae1a8f95d2e
Axhu99 <xhuliodreni@icloud.com>
2024-01-16 17:06:19 +0100

    add snack 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guests = [
+/* const guests = [
     'Brad Pitt',
     'Johnny Depp',
     'Lady Gaga',
@@ -19,10 +19,58 @@ const guestsList = guests.map((guest, i) => {
         nameVip: guest,
         place: i+1
     }
-    /* console.log(guest) */
 
     return guest
 })
 
-console.log(guestsList)
+console.log(guestsList) */
 
+const students = [
+    {
+        Id: 213,
+        name: 'Marco della Rovere',
+        voto: 78
+    },
+    {
+        Id: 110,
+        name: 'Paola Cortellessa',
+        voto: 96
+    },
+    {
+        Id: 250,
+        name: 'Andrea Mantegna',
+        voto: 48
+    },
+    {
+        Id: 145,
+        name: 'Gaia Borromini',
+        voto: 48
+    },
+    {
+        Id: 196,
+        name: 'Luigi Grimaldello',
+        voto: 68
+    },
+    {
+        Id: 102,
+        name: 'Piero della Francesca',
+        voto: 78
+    },
+    {
+        Id: 120,
+        name: 'Francesca da Polenta',
+        voto: 84
+    }
+]
+
+const studentVotoOver = students.filter(({voto}) => voto >= 70)
+
+console.log(studentVotoOver)
+
+const studentVotoIdOver = students.filter((students) => students.voto >= 70 && students.Id >= 120)
+
+console.log(studentVotoIdOver)
+
+const studentNameUppercase = students.map(({name}) => name.toUpperCase())
+
+console.log(studentNameUppercase)
